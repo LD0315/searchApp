4db4652de87b125b86a1fb86b7c34bb1a409b94d
Linlin Deng <linlin.deng@icemedia.com.au>
2023-02-03 17:18:13 +1000

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
+import { Routes } from './components/Routes';
 
 const App = () => {
+    const [darkTheme, setDarkTheme] = useState(false);
+
   return (
-    <div>App</div>
+    <div className={darkTheme ? 'dark' : ''}>
+        <div className="bg-gray-500">
+            App
+        </div>
+    </div>
   )
 }
 
